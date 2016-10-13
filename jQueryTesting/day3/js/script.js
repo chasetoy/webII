9d3bccb513d0279e7d3d77bc37624906b0bb5b7d
chasetoy <chase.a.toy@gmail.com>
2016-10-13 12:55:31 -0400

    update

---
+++ jQueryTesting/day3/js/script.js
@@ -0,0 +1,10 @@
+$(function() {
+    $( "#accordion" ).accordion({
+        collapsible : true
+    });
+
+    $( '.tooltip' ).tooltip();
+    $('#tabs').tabs();
+    $('#datepicker').datepicker();
+    $('#controlgroup').controlgroup();
+});
